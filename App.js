e04e7687cf35849a9c411f484ff6cec2feea4dbc
hanna_nyman <hannanyman.sk@gmail.com>
2020-02-14 13:47:43 +0100

    Player board basics

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { MaterialCommunityIcons as Icon } from 'react-native-vector-icons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <View style={{ flexDirection: 'row' }}>
+          <View style={[styles.tile, { borderLeftWidth: 0, borderTopWidth: 0 }]}>
+            <Icon name="close"/>
+          </View>
+          <View style={[styles.tile, { borderTopWidth: 0, borderLeftWidth: 0 }]}></View>
+          <View style={[styles.tile, { borderWidth: 0, borderBottomWidth: 4 }]}></View>
+        </View>
+
+        <View style={{ flexDirection: 'row' }}>
+          <View style={[styles.tile, { borderLeftWidth: 0, borderTopWidth: 0 }]}></View>
+          <View style={[styles.tile, { borderLeftWidth: 0, borderTopWidth: 0 }]}></View>
+          <View style={[styles.tile, { borderWidth: 0, borderBottomWidth: 4 }]}></View>
+        </View>
+
+        <View style={{ flexDirection: 'row' }}>
+          <View style={[styles.tile, { borderWidth: 0, borderRightWidth: 4 }]}></View>
+          <View style={[styles.tile, { borderWidth: 0, borderRightWidth: 4 }]}></View>
+          <View style={[styles.tile, { borderWidth: 0 }]}></View>
+        </View>
+
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +39,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tile: {
+    borderWidth: 4,
+    height: 100,
+    width: 100,
+  }
 });
