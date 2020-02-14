f8cf20fc8a5958a253f034cc4fc79355df555a41
hanna_nyman <hannanyman.sk@gmail.com>
2020-02-14 14:31:28 +0100

    Tic tac toe game works

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 import { MaterialCommunityIcons as Icon } from 'react-native-vector-icons';
 
 export default class App extends React.Component {
@@ -30,6 +30,38 @@ export default class App extends React.Component {
     })
   }
 
+  getWinner = () => {
+    const num_tiles = 3;
+    let arr = this.state.gameState;
+    let sum;
+
+    // looping through the rows to see if someone has won
+    for (let i = 0; i < num_tiles; i++) {
+      sum = arr[i][0] + arr[i][1] + arr[i][2]
+      if (sum == 3) { return 1; }
+      else if (sum == -3) { return -1; }
+    }
+
+    // looping through the columns to see if someone has won
+    for (let i = 0; i < num_tiles; i++) {
+      sum = arr[0][i] + arr[1][i] + arr[2][i]
+      if (sum == 3) { return 1; }
+      else if (sum == -3) { return -1; }
+    }
+
+    // check the diagonals to see if someone has won
+    sum = arr[0][0] + arr[1][1] + arr[2][2]
+    if (sum == 3) { return 1; }
+    else if (sum == -3) { return -1; }
+
+    sum = arr[2][0] + arr[1][1] + arr[0][2]
+    if (sum == 3) { return 1; }
+    else if (sum == -3) { return -1; }
+
+    // if it ran this far, there is no winner
+    return 0;
+  }
+
   onTilePress(row, col) {
     // if tile is already filled, do not continue
     let value = this.state.gameState[row][col]
@@ -43,6 +75,17 @@ export default class App extends React.Component {
     let arr = this.state.gameState.slice();
     arr[row][col] = currentPlayer;
     this.setState({gameState: arr});
+
+    let winner = this.getWinner();
+
+    // alerting the winner
+    if (winner == 1) {
+      Alert.alert('Player 1 is the winner!!');
+      setTimeout(() => {this.initializeGame();}, 1000)
+    } else if (winner == -1) {
+      Alert.alert('Player 2 is the winner!!');
+      setTimeout(() => {this.initializeGame();}, 1000)
+    }
 
     // switch next player
     let nextPlayer = (currentPlayer == 1) ? -1 : 1
